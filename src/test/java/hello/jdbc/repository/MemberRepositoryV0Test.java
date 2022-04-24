4c2e54d58502a0322140b955b403d5a7987a7c2a
salgu1998 <salgu1998@gmail.com>
2022-04-24 18:39:45 +0900

    add : jdbc - findbyid

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,18 +1,28 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     final MemberRepositoryV0 repo = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        repo.save(new Member("김프링", 1000));
+        // save
+        final Member member = new Member("살구4", 1000);
+        repo.save(member);
+
+        // findById
+        final Member findMember = repo.findById(member.getMemberId());
+        log.info("findMember = {}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 }
