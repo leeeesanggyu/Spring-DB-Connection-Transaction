04416e5f52c165e0ad05e0bb624262f2a8223557
salgu1998 <salgu1998@gmail.com>
2022-05-12 23:18:47 +0900

    add : transaction - transaction template (template callback pattern)

---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -0,0 +1,52 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - transaction template (template callback pattern)
+ */
+@Slf4j
+public class MemberServiceV3_2 {
+
+    private final TransactionTemplate transactionTemplate;
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.transactionTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) {
+        transactionTemplate.executeWithoutResult(status -> {
+            try {
+                accountTransferLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+    }
+
+    private void accountTransferLogic(String fromId, String toId, int money) throws SQLException {
+        final Member fromMember = memberRepository.findById(fromId);
+        final Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromMember.getMemberId(), fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toMember.getMemberId(), toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex"))
+            throw new IllegalStateException("이체 중 예외발생");
+    }
+
+}
---
+++ src/test/java/hello/jdbc/service/MemberServiceV3_2Test.java
@@ -0,0 +1,78 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
+
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * 트랜잭션 - 커넥션 파라미터 방식 동기화
+ */
+class MemberServiceV3_2Test {
+
+    private MemberServiceV3_2 memberService;
+    private MemberRepositoryV3 memberRepository;
+
+    private static final String toMemberId = "이상규";
+    private static final String fromMemberId = "박승진";
+    private static final String errorMemberId = "ex";
+    private static final int money = 5000;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV3(dataSource);
+        memberService = new MemberServiceV3_2(new JdbcTransactionManager(dataSource), memberRepository);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(toMemberId);
+        memberRepository.delete(fromMemberId);
+        memberRepository.delete(errorMemberId);
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        final Member toMember = new Member(toMemberId, money);
+        final Member fromMember = new Member(fromMemberId, money);
+        memberRepository.save(toMember);
+        memberRepository.save(fromMember);
+
+        memberService.accountTransfer(toMember.getMemberId(), fromMember.getMemberId(), money);
+
+        final Member toMemberResult = memberRepository.findById(toMember.getMemberId());
+        final Member fromMemberResult = memberRepository.findById(fromMember.getMemberId());
+        assertThat(toMemberResult.getMoney()).isEqualTo(money-money);
+        assertThat(fromMemberResult.getMoney()).isEqualTo(money+money);
+    }
+
+    @Test
+    @DisplayName("이체 중 예외발생")
+    void accountTransferEx() throws SQLException {
+        final Member toMember = new Member(toMemberId, money);
+        final Member fromMember = new Member(errorMemberId, money);
+        memberRepository.save(toMember);
+        memberRepository.save(fromMember);
+
+        assertThatThrownBy(() -> memberService.accountTransfer(toMember.getMemberId(), fromMember.getMemberId(), money))
+                .isInstanceOf(IllegalStateException.class);
+        final Member toMemberResult = memberRepository.findById(toMember.getMemberId());
+        final Member fromMemberResult = memberRepository.findById(fromMember.getMemberId());
+        assertThat(toMemberResult.getMoney()).isEqualTo(money);
+        assertThat(fromMemberResult.getMoney()).isEqualTo(money);
+    }
+}
